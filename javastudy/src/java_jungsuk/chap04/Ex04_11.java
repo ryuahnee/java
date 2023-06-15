6516c44a87ad92022f75185205590addb4584795
ryuahneee <ryuahneee@gmail.com>
2023-06-15 17:10:42 +0900

    java_jungsuk(chap04_11)

--- javastudy/src/java_jungsuk/chap04/Ex04_11.java
+++ javastudy/src/java_jungsuk/chap04/Ex04_11.java
@@ -9,18 +9,16 @@ public class Ex04_11 {
 		 * 예를 들어 앞의 두 수가 1과 1이라면 그 다음 수는 2가 되고 그 다음 수는 1과 2를 더해서 3이 되어서
 		 * 1,1,2,3,5,8,13,21,... 과 같은 식으로 진행된다 1 과 1 부터 시작하는 피보나치수열의 10번째 수는 무엇인지 계산하는 프로그램을 완성하시오 10 .
 		 */
-			// Fibonnaci 1, 1 . 수열의 시작의 첫 두 숫자를 로 한다
 			int num1 = 1;
 			int num2 = 1;
-			int num3 = 0; // 세번째 값
-			System.out.print(num1+","+num2+",");
+			int num3 = 0; 		
+			System.out.print(num1+","+num2);
 			
 			for (int i = 0 ; i < 8 ; i++ ) {
-					num3 = num1 + num2;
-					System.out.print(num3+",");
-					num2 = num3;
-					num2 = num3 + num2;
-//					System.out.print(num1+","+num2+",");
+					num3 = num1 + num2;				//3. => num1(1)+num2(2)=3
+					System.out.print(","+num3 );	
+					num1 = num2;				// 최초 반복 시작시 : 1.num2의 값(1) -> num1  / 2.num3의 값(2) -> num2	 
+					num2 = num3;				
 			}
 
 	}
