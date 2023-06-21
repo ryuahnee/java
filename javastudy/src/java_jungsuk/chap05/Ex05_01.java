f85c90468780fce34192a811d7f55b55a364e224
ryuahneee <ryuahneee@gmail.com>
2023-06-21 17:04:04 +0900

    23-06-21 commit

---
+++ javastudy/src/java_jungsuk/chap05/Ex05_01.java
@@ -0,0 +1,22 @@
+package java_jungsuk.chap05;
+
+public class Ex05_01 {
+
+	public static void main(String[] args) {
+		//[5-1] 다음은 배열을 선언하거나 초기화한 것이다 잘못된 것을 고르고 그 이유를 설명 하시오.
+		
+		/*
+		 * a. int[] arr[];
+		 * b. int[] arr = {1,2,3,}; ok
+		 * c. int[] arr = new int[5]; ok
+		 * d. int[]
+		 * arr = new int[5]{1,2,3,4,5};
+		 * e. int arr[5]; ok
+		 * f. int[] arr[] = new int[3][];
+		 */
+		
+		//답 :  d,e
+		
+	}
+
+}
---
+++ javastudy/src/java_jungsuk/chap05/Ex05_02.java
@@ -0,0 +1,21 @@
+package java_jungsuk.chap05;
+
+public class Ex05_02 {
+
+	public static void main(String[] args) {
+		
+		//[5-2] 다음과 같은 배열이 있을 때 arr[3].length 의 값은 얼마인가  ?
+		
+				int[][] arr = {
+				{ 5, 5, 5, 5, 5},
+				{ 10, 10, 10},
+				{ 20, 20, 20, 20},
+				{ 30, 30}
+				};
+				
+//				답 : 2
+
+				
+	}
+
+}
